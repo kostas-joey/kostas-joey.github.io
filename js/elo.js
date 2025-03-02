b785952bbec3327d6dbc3523354ef024df96693e
Konstantinos Tsilimparis <konstantinos@artinis.com>
2025-03-03 00:37:52 +0100

    Update elo.js

--- js/elo.js
+++ js/elo.js
@@ -26,25 +26,42 @@ const db = getFirestore(app);
      return playerRating + K * (result - expectedScore);
  }
  
- function updatePlayerData(playerRatings, playerName, newRating, isWinner) {
-     if (!playerRatings[playerName]) {
-         playerRatings[playerName] = {
-             rating: 1200,
-             matches: 0,
-             wins: 0,
-             losses: 0
-         };
-     }
+//  function updatePlayerData(playerRatings, playerName, newRating, isWinner) {
+//      if (!playerRatings[playerName]) {
+//          playerRatings[playerName] = {
+//              rating: 1200,
+//              matches: 0,
+//              wins: 0,
+//              losses: 0
+//          };
+//      }
      
-     playerRatings[playerName].rating = newRating;
-     playerRatings[playerName].matches += 1;
+//      playerRatings[playerName].rating = newRating;
+//      playerRatings[playerName].matches += 1;
      
-     if (isWinner) {
-         playerRatings[playerName].wins += 1;
-     } else {
-         playerRatings[playerName].losses += 1;
-     }
- }
+//      if (isWinner) {
+//          playerRatings[playerName].wins += 1;
+//      } else {
+//          playerRatings[playerName].losses += 1;
+//      }
+//  }
+
+async function updatePlayerData(playerRatings, playerName, newRating, isWinner) {
+    if (!playerRatings[playerName]) {
+        playerRatings[playerName] = { rating: 1200, matches: 0, wins: 0, losses: 0 };
+    }
+
+    playerRatings[playerName].rating = newRating;
+    playerRatings[playerName].matches += 1;
+    if (isWinner) {
+        playerRatings[playerName].wins += 1;
+    } else {
+        playerRatings[playerName].losses += 1;
+    }
+
+    await savePlayerData(playerRatings);  // Save updated data to Firebase
+}
+
  
  function averageTeamElo(player1Rating, player2Rating) {
      return (player1Rating + player2Rating) / 2;
